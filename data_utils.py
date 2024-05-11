3278a26e9ec11c72b8c842fea6a8acb97a7edac4
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2024-05-11 15:12:50 +0000

    experiment on undersampled data

--- data_utils.py
+++ data_utils.py
@@ -22,7 +22,25 @@ def load_main_data():
   main_df = main_df.drop_duplicates()
   return main_df
   
-def scaffold_split(dataset, seed=326, x_col='smiles', y_col='HIV_active', id_col='smiles'):
+def scaffold_split(dataset, seed=326, x_col='smiles', y_col='HIV_active', id_col='smiles', undersampling=False):
+  if undersampling:
+    dataset = undersample(dataset, seed, x_col, y_col, id_col)
+  else:
+    dataset = sample(dataset, x_col, y_col, id_col)
+    
+  scaffoldsplitter = dc.splits.ScaffoldSplitter()
+  return scaffoldsplitter.train_valid_test_split(dataset, seed=seed, frac_train=.7, frac_valid=.2, frac_test=.1)
+
+def random_split(dataset, seed=326, x_col='smiles', y_col='HIV_active', id_col='smiles', undersampling=False):
+  if undersampling:
+    dataset = undersample(dataset, seed, x_col, y_col, id_col)
+  else:
+    dataset = sample(dataset, x_col, y_col, id_col)
+    
+  randomsplitter = dc.splits.RandomSplitter()
+  return randomsplitter.train_valid_test_split(dataset, seed=seed, frac_train=.7, frac_valid=.2, frac_test=.1)
+  
+def sample(dataset, x_col='smiles', y_col='HIV_active', id_col='smiles'):
   Xs = dataset[x_col].values
   Ys = dataset[y_col].values
   ids = dataset[id_col].values
@@ -32,7 +50,34 @@ def scaffold_split(dataset, seed=326, x_col='smiles', y_col='HIV_active', id_col
   negative_count = sum(Ys == False)
   for i, y in enumerate(Ys):
     W[i] = negative_count/positive_count if y else 1
+
+  dataset = dc.data.NumpyDataset(X=Xs, y=Ys, w=W, ids=ids)
+  return dataset
+
+def undersample(dataset, seed, x_col='smiles', y_col='HIV_active', id_col='smiles'):
+  positive_count = dataset[y_col].sum()
+  negative_count = len(dataset) - positive_count
+  f = positive_count / negative_count
   
-  dataset = dc.data.NumpyDataset (X=Xs, y=Ys, w=W, ids=ids)
-  scaffoldsplitter = dc.splits.ScaffoldSplitter()
-  return scaffoldsplitter.train_valid_test_split(dataset, seed=seed, frac_train=.7, frac_valid=.2, frac_test=.1)
+  negative_df = dataset[dataset[y_col].isin([False, 0])]
+  # negative_df = negative_df.reset_index()
+  # negative_ds = dc.data.NumpyDataset(X=negative_df['index'].values, y=negative_df[y_col].values, ids=negative_df[id_col].values)
+  # scaffoldsplitter = dc.splits.ScaffoldSplitter()
+  # samples, rest = scaffoldsplitter.train_test_split(negative_ds, seed=seed, frac_train=f)
+  
+  # positive_df = dataset[dataset[y_col].isin([True, 1])]
+  # all_samples = dc.data.NumpyDataset(
+  #   X=[*negative_df[negative_df['index'].isin(samples.X)][x_col], *positive_df[x_col].values],
+  #   y=[*samples.y, *positive_df[y_col].values],
+  #   ids=[*negative_df[negative_df['index'].isin(samples.X)][id_col], *positive_df[id_col].values]
+  # )
+  
+  negative_samples = negative_df.sample(frac=f, random_state=seed)
+  positive_samples = dataset[dataset[y_col].isin([True, 1])]
+  all_samples = dc.data.NumpyDataset(
+    X=np.array([*negative_samples[x_col], *positive_samples[x_col]]),
+    y=np.array([*negative_samples[y_col], *positive_samples[y_col]]),
+    ids=np.array([*negative_samples[id_col], *positive_samples[id_col]])
+  )
+  
+  return all_samples
